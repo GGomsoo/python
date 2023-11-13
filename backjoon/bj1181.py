7d9f6dd22ea18da39cd0ccfe68c848ec4384a622
Park Jong Hyuk <occultism22@naver.com>
2023-11-13 21:39:38 +0900

    231113

---
+++ backjoon/bj1181.py
@@ -0,0 +1,25 @@
+# 단어 정렬
+# 실버 V
+
+# 소문자로 이루어진 N개의 단어 들어오면
+# 1. 길이가 짧은 것 부터
+# 2. 길이가 같다면 사전 순으로
+
+# N = int(input())
+# ans = []
+
+# for _ in range(N):
+#     word = input()
+#     ans.append(word)
+
+# for i in range(len(ans)):
+#     for i in range(len(ans)-1):
+#         if len(ans[i]) > len(ans[i+1]):
+#             ans[i], ans[i+1] = ans[i+1], ans[i]
+
+#         # else: 
+#         #     if len(ans[i]) == len(ans[i+1]):
+#         #         if ord(ans[i][0]) > ord(ans[i+1][0]):
+#         #             ans[i], ans[i+1] = ans[i+1], ans[i]
+
+print('다시 풀어봐야 해')
