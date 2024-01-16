0be054fbd9be12afaff84b329259a193a1905c99
Park Jong Hyuk <occultism22@naver.com>
2024-01-17 01:28:33 +0900

    백준1259 팰린드롬수

--- backjoon/bj1259.py
+++ backjoon/bj1259.py
@@ -5,11 +5,25 @@ while True:
     if int(N) == 0:
         break
     
-    # 홀수
+    # 입력값의 길이가 홀수
     if M // 2 != 0:
+        flag = 1
         for i in range(M//2 + 1):
-            if N[i] == N[M-i-1]:
-                print('yes')
-            else:
+            if N[i] != N[M-i-1]:
+                flag = 0
                 print('no')
                 break
+        
+        if flag:
+            print('yes')
+
+    # 입력값의 길이가 짝수
+    if M // 2 == 0:
+        flag = 1
+        for i in range(M//2):
+            if N[i] != N[M-1-i]:
+                flag = 0
+                print('no')
+                break
+        if flag:
+            print('yes')
