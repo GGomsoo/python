424fccb7084c6187b33c07d08df4f460b066ef0f
parkjonghyuk <occultism22@naver.com>
2024-01-20 01:49:41 +0900

    백준2775 푸는중

---
+++ backjoon/bj2775.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for _ in range(T):
+    K = int(input())
+    N = int(input())
+    
+    
+    for i in range(K+1):
+        lst = []
+        people = 1
+        for j in range(1, N+1):
+            lst.append(j)
+            
+        print(lst)
