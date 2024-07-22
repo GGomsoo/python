1c3551eb686462cf829f3325b1940e72991e7439
Park Jong Hyuk <occultism22@naver.com>
2024-07-22 23:55:46 +0900

    240722/백준10699-오늘날짜

--- backjoon/bj10699.py
+++ backjoon/bj10699.py
@@ -1,3 +1,11 @@
 from datetime import datetime
 
 print(datetime.today().strftime("%Y-%m-%d"))
+
+# 현재 날짜 가져오기
+# datetime.today() 오늘 날짜
+# datetime.today().year 현재 연도
+# datetime.today().month 현재 월
+# datetime.today().day 현재 일
+# datetime.today().hour 현재 시간 
+
