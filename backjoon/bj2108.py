9c4cc3b2a825880e5989d148bb953a58dcc536c8
Park Jong Hyuk <occultism22@naver.com>
2024-07-23 00:08:50 +0900

    240723/백준2108-통계학 (진행중)

--- backjoon/bj2108.py
+++ backjoon/bj2108.py
@@ -4,3 +4,25 @@
 # 최빈값 : N개의 수들 중 가장 많이 나타나는 값
 # 범위 : N개의 수들 중 최댓값과 최솟값의 차이
 import sys
+
+N = int(sys.stdin.readline())
+arr = []
+
+for _ in range(N):
+    arr.append(int(sys.stdin.readline()))
+arr.sort()
+
+lst = [0] * (max(arr)+1)
+# 산술 평균
+print(sum(arr) // N)
+
+# 중앙값
+print(arr[N // 2])
+
+# 최빈값
+for i in arr:
+    lst[i] += 1
+print(lst.index(max(lst))) # 리스트에서 인덱스를 찾는 함수 'index'
+
+# 최댓값과 최솟값의 차이
+print(max(arr) - min(arr))
