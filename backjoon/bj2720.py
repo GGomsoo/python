3dbe1e7e44c576552e7806ae9350aea837417fe7
Park Jong Hyuk <occultism22@naver.com>
2024-08-08 23:21:43 +0900

    240808/백준2720-세탁소사장동혁

--- backjoon/bj2720.py
+++ backjoon/bj2720.py
@@ -1,14 +1,13 @@
 tc = int(input())
 
-changes = [25, 10, 5, 1]
+changes = [25, 10, 5, 1] # 잔돈 종류
 
 for i in range(tc):
-    money = int(input())
+    money = int(input()) # 거스름돈의 액수
+    ans = [0, 0, 0, 0] # 각각 줘야하는 동전의 갯수
 
-    while money == 0:
-        ans = [0, 0, 0, 0]
-
-        for i in range(len(ans)):
-            ans += money // changes[i]
-            money %= changes[i]
-        print(*ans, end=" ")
+    for i in range(4):
+        ans[i] += money // changes[i] # 거스름돈을 잔돈으로 나눈 몫 == 갯수를 list에 추가
+        money %= changes[i] # 남은 거스름돈
+    
+    print(*ans)
