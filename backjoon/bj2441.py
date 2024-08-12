0798841b7eaa42480bc4878b0d20f9f48fe47dad
Park Jong Hyuk <occultism22@naver.com>
2024-08-12 21:38:28 +0900

    240812/백준-별찍기

---
+++ backjoon/bj2441.py
@@ -0,0 +1,6 @@
+# 별찍기 -4
+
+N = int(input())
+
+for i in range(N):
+    print(" " * i + "*" * (N-i))
--- backjoon/bj2444.py
+++ backjoon/bj2444.py
@@ -1,13 +1,10 @@
 # 별 찍기 -7
 # 브론즈 III
 
-# 별찍기인데 좀 더 생각하고 풀어야 할 문제
-
 N = int(input())
 
-for i in range(N*2):
-    if i < N:
-        print(' ' * (N-i) + '*' * (i*2+1) + ' ' * (N-i))
-    else:
-        print(' ' * (i-N) + '*' * (N*2+1-i) + ' ' * (i-N))
-print()
+for i in range(N):
+    print(' ' * (N-i) + '*' * (i*2+1))
+
+for i in range(N-2, -1, -1):
+    print(' ' * (N-i) + '*' * (i*2+1))
